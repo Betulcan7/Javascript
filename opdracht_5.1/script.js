02578304b0dbfbed17d2c65d84af9edca19fc8c0
Betulcan7 <112864181+Betulcan7@users.noreply.github.com>
2022-10-12 18:21:51 +0200

    Opdracht_5.1

---
+++ opdracht_5.1/script.js
@@ -0,0 +1,16 @@
+alert("Hallo wereld");
+
+document.getElementById("title").style.color = "red";
+
+
+
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 * getal2;
+alert(getal1 + " x " + getal2 + "=" + som);
+
+var kleur = prompt("Geef een Engelse kleurnaam:", "");
+
+document.getElementById("title").style. color = kleur;
+
+document.getElementById("p1").innerHTML = "Hello " + kleur + "!";

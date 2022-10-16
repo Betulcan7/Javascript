25db1e903465eafb93118e7301d962dc8a81d1f1
Betulcan7 <112864181+Betulcan7@users.noreply.github.com>
2022-10-16 17:57:13 +0200

    Opdracht_6.1

---
+++ opdracht_6.1/script.js
@@ -0,0 +1,18 @@
+function kleur_aanpassen(){
+
+var kleur = prompt("Geef een Engelse kleurnaam:", "");
+document.getElementById("title").style. color = kleur;
+
+}
+
+kleur_aanpassen();
+
+
+
+function changeText(){
+
+  document.getElementById("demo").innerHTML=("Inhoud aangepast!");
+}
+
+
+

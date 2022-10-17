15b46686ea14667e0f7c44affe8c58383eb97e53
Betulcan7 <112864181+Betulcan7@users.noreply.github.com>
2022-10-17 18:58:48 +0200

    Opdracht_6.2

---
+++ opdracht_6.2/script.js
@@ -0,0 +1,10 @@
+
+    function changeText(){
+    
+      document.getElementById("demo").innerHTML=("Inhoud aangepast!");
+    }
+    
+    
+    function myFunction() {
+        document.getElementById("demo2").innerHTML = "Hello World";
+      }

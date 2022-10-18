f8f5bdbc10e1031bf5962e704f9ff4191428a775
Betulcan7 <112864181+Betulcan7@users.noreply.github.com>
2022-10-18 16:14:40 +0200

    opdracht_6.3

---
+++ opdracht_6.3/script.js
@@ -0,0 +1,12 @@
+
+    function changeText(){
+    
+        document.getElementById("demo").innerHTML=("Inhoud aangepast!");
+      }
+      
+      
+    
+
+        function myFunction() {
+            document.getElementById("demo3").innerHTML = "YOU CLICKED ME!";
+          }

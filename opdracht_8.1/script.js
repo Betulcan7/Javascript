86f10c15aa3e68dabb315a04090104595a23cce3
Betulcan7 <112864181+Betulcan7@users.noreply.github.com>
2022-11-01 15:46:06 +0100

    opdracht_8.1

---
+++ opdracht_8.1/script.js
@@ -0,0 +1,27 @@
+function add(){
+    nummer1=parseInt(myform.nummer1.value);
+    nummer2=parseInt(myform.nummer2.value);
+    result=nummer1+nummer2
+    myform.result.value=result;
+}
+
+function minus(){
+    nummer1=parseInt(myform.nummer1.value);
+    nummer2=parseInt(myform.nummer2.value);
+    result=nummer1-nummer2
+    myform.result.value=result;
+}
+
+function multiply(){
+    nummer1=parseInt(myform.nummer1.value);
+    nummer2=parseInt(myform.nummer2.value);
+    result=nummer1*nummer2
+    myform.result.value=result;
+}
+
+function devide(){
+    nummer1=parseInt(myform.nummer1.value);
+    nummer2=parseInt(myform.nummer2.value);
+    result=nummer1/nummer2
+    myform.result.value=result;
+}
